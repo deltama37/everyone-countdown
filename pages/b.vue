bb91534bc9f303c4648f739488c0500e35855403
Ryota Misumi <3delta7@gmail.com>
2019-04-30 11:11:22 +0900

    update package

--- pages/b.vue
+++ pages/b.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.form  
+.form
   .top
     img(src="~assets/top.png")
   .countup
@@ -16,7 +16,7 @@
       p.medium 分
       p.big-big 45
       p.medium 秒
-      p.medium 67  
+      p.medium 67
   .areas
     img(src="~assets/bg_area.png")
     img.center(src="~assets/bg_area.png")
@@ -33,14 +33,14 @@
         p.name みすみ
   .hold-on
     img(src="~assets/icon.png")
-    img.center(src="~assets/icon.png") 
+    img.center(src="~assets/icon.png")
     img(src="~assets/icon.png")
   .count
     p 11
     p.center 11
-    p 11 
+    p 11
   .button
-    img(src="~assets/btn_black.png")    
+    img(src="~assets/btn_black.png")
 </template>
 
 <script>
@@ -63,9 +63,9 @@ export default {
     margin-bottoom: 17px
     position: relative
     & img
-      width: 750px   
+      width: 750px
   & .year
-    display: flex 
+    display: flex
     justify-content: center
     position: absolute
     flex-derection: row
@@ -76,7 +76,7 @@ export default {
     color: white
     font-family: vdl-logojrblack, sans-serif
   & .time
-    display: flex 
+    display: flex
     justify-content: center
     position: absolute
     top: 50%
@@ -88,37 +88,37 @@ export default {
   & .small
     font-size: 42px
   & .big-big
-    font-size: 76px  
+    font-size: 76px
   & .medium
     font-size: 46px
   & .areas
     display: flex
-    justify-content: center  
-    position: relative  
+    justify-content: center
+    position: relative
     & img
       width: 210px
       height: 850px
     & .center
-      margin-right: 17px  
-      margin-left: 17px  
+      margin-right: 17px
+      margin-left: 17px
   & .content
-    display: flex 
+    display: flex
     justify-content: center
-  & .content-left  
+  & .content-left
     position: absolute
     top: 10%
     left: 8%
     writing-mode: vertical-rl
     // width: 180px
-  & .content-center 
+  & .content-center
     position: absolute
     top: 10%
-    left: 38%  
+    left: 38%
     writing-mode: vertical-rl
   & .content-right
     position: absolute
     top: 10%
-    left: 67%  
+    left: 67%
     writing-mode: vertical-rl
   & .main
     font-size: 50px
@@ -126,14 +126,14 @@ export default {
     text-align: left
     height: 600px
   & .name
-    font-size: 20px  
+    font-size: 20px
     height: 600px
-    text-align: right    
+    text-align: right
   & .count
     display: flex
     justify-content: center
-    margin-top: -200px 
-    position: relative   
+    margin-top: -200px
+    position: relative
     & p
       font-size: 50px
       font-style: oblique
@@ -143,15 +143,15 @@ export default {
       margin-left: 180px
   & .hold-on
     display: flex
-    justify-content: center  
-    margin-top: -70px 
+    justify-content: center
+    margin-top: -70px
     position: relative
     & img
       width: 210px
       height: 130px
-    & .center  
+    & .center
       margin-right: 17px
-      margin-left: 17px  
+      margin-left: 17px
   & .button
     position: fixed
     bottom: 20px
@@ -159,5 +159,5 @@ export default {
     right: 25px
     padding-bottom: 100px
     & img
-      width: 700px         
+      width: 700px
 </style>
